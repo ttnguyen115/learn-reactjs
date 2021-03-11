7f324773a53c225d578bf75e521bb7b2f96d16a0
ttnguyen115 <thanhtrung1012000@gmail.com>
2021-03-11 12:52:03 +0700

    render elements in react

--- src/App.js
+++ src/App.js
@@ -1,22 +1,61 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const name = 'Trung';
+  const age = 21;
+  const isMale = true;
+  const student = {
+    name: 'Easy Frontend',
+  };
+  const colors = ['red', 'green', 'blue'];
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          It's me again, Trung Nguyen (test SSH) the third times
+          It's me again, { name } - { age } - { isMale ? 'Male' : 'No' } (test SSH) the four times
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-    
-        </a>
+        { isMale ? <p>Trung</p> : <p>21</p> }
+
+        <p>------------------------------</p>
+
+        { isMale && <p>Trung</p> }
+        { !isMale && <p>21</p> }
+
+        <p>------------------------------</p>
+
+        { isMale && (
+          <div>
+            <h2>Trung 1</h2>
+            <h2>Trung 1</h2>
+            <h2>Trung 1</h2>
+          </div>
+        ) }
+
+        <p>------------------------------</p>
+
+        { isMale && (
+          <>
+            <h2>Trung 2</h2>
+            <h2>Trung 2</h2>
+            <h2>Trung 2</h2>
+          </>
+        ) }
+
+        <p>------------------------------</p>
+
+        <p>{ student.name }</p>
+
+        <p>------------------------------</p>
+
+        <ul>
+          {colors.map(color => (
+            <li key={ color } style={{ color }}>{color}</li>
+          ))}
+        </ul>
       </header>
     </div>
   );
