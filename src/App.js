58e31d3e1f8d69478328a9c87c4e4bf8be3bca94
ttnguyen115 <thanhtrung1012000@gmail.com>
2021-03-11 10:35:11 +0700

    updated title

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          It's me again, Trung Nguyen
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          It's me again, Trung Nguyen
+    
         </a>
       </header>
     </div>
