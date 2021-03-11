e8cd107c57fc2ae9eee49d4e052d1299f04359a0
ttnguyen115 <thanhtrung1012000@gmail.com>
2021-03-11 10:30:55 +0700

    updated

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          It's me again, Trung Nguyen
         </a>
       </header>
     </div>
