254cc5476982c3911aabe018635eb3e23874156e
ttnguyen115 <thanhtrung1012000@gmail.com>
2021-03-11 10:43:58 +0700

    test SSH

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          It's me again, Trung Nguyen
+          It's me again, Trung Nguyen (test SSH)
         </p>
         <a
           className="App-link"
